db8557f57983bb7ed7f1b67c5b3c1279d7aa2124
Artur Hebda <arturhebda@gmail.com>
2012-12-15 19:48:27 +0100

    Wczytywanie parametrów do obiektu RailwayStation zarówno ze standardowego wejścia, jak i z pliku.

--- src/railwaystation/RailwayStation.java
+++ src/railwaystation/RailwayStation.java
@@ -5,6 +5,19 @@
 package railwaystation;
 
 import desmoj.core.simulator.Model;
+import desmoj.core.simulator.TimeSpan;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import railwaystation.infrastructure.CashDeskRegion;
 import railwaystation.infrastructure.Infrastructure;
 import railwaystation.infrastructure.Region;
@@ -15,7 +28,102 @@ import railwaystation.infrastructure.ServingRegion;
  * @author artur
  */
 public class RailwayStation extends Model {
-    protected Infrastructure infrastructure;
+    public TimeSpan minGoToWaitingRoom, maxGoToPlatform, externalDelayInfoSpan;
+    public TimeSpan minComingTimeWithTicket, maxComingTimeWithTicket, minComingTimeWithoutTicket, maxComingTimeWithoutTicket;
+    public double complainingProbability, havingCompanionProbability, havingTicketProbability, shareOfVisitors;
+    public int platformCount, cashDeskCount, infoDeskCount, waitingRoomCapacity;
+    public int minArrivingPassengerCount, maxArrivingPassengerCount, minDeparturingPassengerCount, maxDeparturingPassengerCount; // per train
+    public TimeSpan minInternalArrivalTime, maxInternalArrivalTime; // ride from semaphore to the platform
+    public TimeSpan minSellingTicketTime, maxSellingTicketTime, minServingInformationTime, maxServingInformationTime;
+    
+    public CrowdSpeed crowdSpeed;
+    public SchedulingAlgorithm schedulingAlgorithm;
+    
+    private Infrastructure infrastructure;
+    
+    private class ConfigurationManager {
+        protected InputStream stream;
+        protected JSONObject config;
+
+        public ConfigurationManager(InputStream stream) {
+            this.stream = stream;
+            this.config = null;
+        }
+
+        public void read() {
+            InputStreamReader inputReader = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+                String line;
+                StringBuilder builder = new StringBuilder();
+                inputReader = new InputStreamReader(stream);
+                bufferedReader = new BufferedReader(inputReader);
+
+                while((line = bufferedReader.readLine()) != null) {
+                    builder.append(line);
+                }
+
+                try {
+                    this.config = new JSONObject(builder.toString());
+                } catch (JSONException ex) {
+                    System.err.println("error reading configuration json");
+                }
+
+            } catch (IOException ex) {
+                System.err.println("error reading configuration stream");
+            } finally {
+                try {
+                    if (bufferedReader != null) {
+                        bufferedReader.close();
+                    }
+                    if (inputReader != null) {
+                        inputReader.close();
+                    }
+                } catch (IOException ex) {}
+            }
+        }
+
+        public void setParameters() {
+            if (config != null) {
+                minGoToWaitingRoom = new TimeSpan(config.optInt("go_to_waiting_room_min_time_span", 20), TimeUnit.MINUTES);
+                maxGoToPlatform = new TimeSpan(config.optInt("go_to_platform_max_time_span", 10), TimeUnit.MINUTES);
+                externalDelayInfoSpan = new TimeSpan(config.optInt("external_delay_info_time_span", 30), TimeUnit.MINUTES);
+                
+                minComingTimeWithTicket = new TimeSpan(config.optInt("min_coming_time_span_with_ticket", 15), TimeUnit.MINUTES);
+                maxComingTimeWithTicket = new TimeSpan(config.optInt("max_coming_time_span_with_ticket", 45), TimeUnit.MINUTES);
+                minComingTimeWithoutTicket = new TimeSpan(config.optInt("min_coming_time_span_without_ticket", 25), TimeUnit.MINUTES);
+                maxComingTimeWithoutTicket = new TimeSpan(config.optInt("max_coming_time_span_without_ticket", 50), TimeUnit.MINUTES);
+
+                complainingProbability = config.optInt("average_probability_of_complaining", 15) / 100.0;
+                havingCompanionProbability = config.optInt("average_probability_of_having_companion", 25) / 100.0;
+                havingTicketProbability = config.optInt("average_probability_of_having_ticket", 50) / 100.0;
+                shareOfVisitors = config.optInt("average_share_of_visitors", 25) / 100.0;
+
+                platformCount = config.optInt("platform_count", 4);
+                infoDeskCount = config.optInt("info_desk_count", 2);
+                cashDeskCount = config.optInt("cash_desk_count", 6);
+                waitingRoomCapacity = config.optInt("waiting_room_capacity", 1000);
+
+                minArrivingPassengerCount = config.optInt("min_arriving_passenger_count", 10);
+                maxArrivingPassengerCount = config.optInt("max_arriving_passenger_count", 800);
+                minDeparturingPassengerCount = config.optInt("min_departuring_passenger_count", 10);
+                maxDeparturingPassengerCount = config.optInt("max_departuring_passenger_count", 800);
+
+                minInternalArrivalTime = new TimeSpan(config.optInt("min_internal_arrival_time", 5), TimeUnit.MINUTES);
+                maxInternalArrivalTime = new TimeSpan(config.optInt("max_internal_arrival_time", 20), TimeUnit.MINUTES);
+
+                minSellingTicketTime = new TimeSpan(config.optInt("min_selling_ticket_time", 5), TimeUnit.MINUTES);
+                maxSellingTicketTime = new TimeSpan(config.optInt("max_selling_ticket_time", 10), TimeUnit.MINUTES);
+                minServingInformationTime = new TimeSpan(config.optInt("min_serving_information_time", 5), TimeUnit.MINUTES);
+                maxServingInformationTime = new TimeSpan(config.optInt("max_serving_information_time", 15), TimeUnit.MINUTES);
+                
+                /*
+                    crowdSpeed, schedulingAlgorithm
+                 */
+            }
+        }
+    }
 
     public RailwayStation() {
         super(null, "railway-station", true, true);
@@ -25,6 +133,7 @@ public class RailwayStation extends Model {
      */
     public static void main(String[] args) {
         RailwayStation station = new RailwayStation();
+        station.readConfig(args);
     }
 
     protected void buildInfrastructure() {
@@ -65,5 +174,29 @@ public class RailwayStation extends Model {
     public void init() {
         buildInfrastructure();
         // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void readConfig(String[] args) {
+        InputStream configStream = null;
+
+        if (args.length == 2 && args[0].equals("-i") && args[1].equals("STDIN")) {
+            configStream = System.in;
+        } else {
+            try {
+                File configFile = new File("config.json");
+                configStream = new FileInputStream(configFile);
+            } catch (FileNotFoundException ex) {
+                System.err.println("configuration file not found!");
+            }
+        }
+
+        ConfigurationManager config = new ConfigurationManager(configStream);
+        config.read();
+        if (configStream != null) {
+            try {
+                configStream.close();
+            } catch (IOException ex) {}
+        }
+        config.setParameters();
     }
 }
