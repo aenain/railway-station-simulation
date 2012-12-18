80fa1fb880f0db26669be34a729cd1ce6d193fb6
Artur Hebda <arturhebda@gmail.com>
2012-12-18 02:31:51 +0100

    Dodanie klasy JSONWriter do zapisu jsonów.

---
+++ src/railwaystation/io/JSONWriter.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package railwaystation.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author artur
+ */
+public class JSONWriter {
+    public static void write(File file, JSONObject json) {
+        FileOutputStream stream = null;
+        try {
+            stream = new FileOutputStream(file);
+            write(stream, json);
+        } catch (FileNotFoundException ex) {
+            System.err.println("file not found");
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (IOException ex) {}
+        }
+    }
+
+    public static void write(OutputStream stream, JSONObject json) {
+        OutputStreamWriter outputWriter = null;
+
+        try {
+            outputWriter = new OutputStreamWriter(stream);
+            outputWriter.write(json.toString());
+        } catch (IOException ex) {
+            System.err.println("error writing json stream");
+        } finally {
+            try {
+                if (outputWriter != null) {
+                    outputWriter.close();
+                }
+            } catch (IOException ex) {}
+        }
+    } 
+}
